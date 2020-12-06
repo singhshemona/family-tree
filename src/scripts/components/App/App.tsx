2658f3450e2bd4f8b9921c52ec9ce5320576846e
Shemona Singh <singhshemona@gmail.com>
2020-12-06 12:51:45 -0500

    found 3 wrong ways to implement nodeThreeObject

--- src/scripts/components/App/App.tsx
+++ src/scripts/components/App/App.tsx
@@ -2,24 +2,46 @@ import React from 'react';
 import '../../../styles/index.scss';
 import ForceGraph3D from 'react-force-graph-3d';
 import { data } from '../../data.js';
+import SpriteText from 'three-spritetext';
 
 export const App = (): React.ReactElement => {
   return (
     <div className="App">
       <ForceGraph3D
         graphData={data}
-        linkDirectionalArrowLength={3}
+        linkDirectionalArrowLength={5}
         linkDirectionalArrowRelPos={1}
         linkWidth={0.5}
-        nodeAutoColorBy="group"
-        // onNodeDragEnd={(node) => {
-        //   node.fx = node.x;
-        //   node.fy = node.y;
-        //   node.fz = node.z;
+        onNodeDragEnd={(node) => {
+          node.fx = node.x;
+          node.fy = node.y;
+          node.fz = node.z;
+        }}
+        backgroundColor="#f6f5f5"
+        linkColor={() => '#rgba(7, 13, 89, 1)'}
+        nodeColor={() => '#rgba(31, 60, 136, 1)'}
+        // nodeThreeObject={(data) => {
+        //   const sprite = new SpriteText('test');
+        //   sprite.color = 'red';
+        //   return sprite;
         // }}
-        backgroundColor="#f7f7f7"
-        linkColor={() => 'rgba(255,0,255,1)'}
-        d3VelocityDecay={0.3}
+        // nodeThreeObject={new SpriteText('text', 8, 'blue')}
+        // nodeThreeObject={(node) => {
+        //   let i = 0;
+        //   let sprite: SpriteText = new SpriteText();
+        //   while (i < data.nodes.length) {
+        //     sprite = new SpriteText(data.nodes[i].name);
+        //     sprite.color = 'red';
+        //     i++;
+        //   }
+        //   return sprite;
+        // }}
+        nodeThreeObject={(node) => {
+          const sprite = new SpriteText(`${node.id}`);
+          sprite.color = '#1f3c88';
+          sprite.textHeight = 8;
+          return sprite;
+        }}
       />
     </div>
   );
--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,26 +1,43 @@
 export const data = {
   nodes: [
     {
-      id: 'babitaTilak',
-      name: 'Babita Singh / Tilak Singh',
+      id: 'Babita Singh / Tilak Singh',
+      name: 'babitaTilak',
+      // val: 1,
     },
     {
       id: 'shemona',
       name: 'Shemona Singh',
+      // val: 10,
     },
     {
       id: 'taniya',
       name: 'Taniya Singh',
+      // val: 20,
+    },
+    {
+      id: 'sally',
+      name: 'sally Singh',
+      // val: 20,
+    },
+    {
+      id: 'sam',
+      name: 'sam Singh',
+      // val: 20,
     },
   ],
   links: [
     {
-      source: 'babitaTilak',
+      source: 'Babita Singh / Tilak Singh',
       target: 'shemona',
     },
     {
-      source: 'babitaTilak',
+      source: 'Babita Singh / Tilak Singh',
       target: 'taniya',
+    },
+    {
+      source: 'sally',
+      target: 'sam',
     },
   ],
 };
